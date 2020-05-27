695ce4419ca4cb85d00b94cef9545fc079fbed10
Wyatt Evans <syattevans@gmail.com>
2020-05-27 02:00:12 -0500

    Optimized imports and added options menu.

--- app/src/main/java/com/app/basicactivity/MainActivity.kt
+++ app/src/main/java/com/app/basicactivity/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.app.basicactivity
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
-import android.view.MenuItem
-
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,5 +17,10 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
     }
 }
